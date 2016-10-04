bb5ddcf923c0528cb987525c7241e500eb7bb300
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2016-10-04 16:03:08 +0300

    fixed pca.R scrupt

--- r_scripts/analysis/pca.R
+++ r_scripts/analysis/pca.R
@@ -1,4 +1,11 @@
 #!/usr/bin/Rscript
+# 
+# Author: ggian
+# Last updated: Tue Oct  4 16:02:00 EEST 2016
+# This script analyzes an input dataset, and outputs the eigenvectors
+# of the dataset, scaled by their respective variance (as determined by 
+# the respective eigenvalue)
+
 args <- commandArgs(trailingOnly=TRUE)
 datafile <- args[1]
 dataset <- read.csv(datafile)
@@ -11,16 +18,16 @@ cat(length(fit$sd)^2)
 cat("\n")
 
 # prints the eigenvalues
-#for (i in 1:(cols)) {
-#	cat(as.numeric(fit$sd[i]^2))
-#	cat(" ")
-#}
-#cat("\n")
+
+variances=c()
+sumOfVariances = sum(as.numeric(fit$sd)^2)
+for (i in 1:(cols)) {
+	variances[i] = (fit$sd[i]^2)/sumOfVariances
+}
 
 # prints the eigenvectors
 for (i in 1:(cols*cols)) {
-	v <- as.numeric(fit$sd[(i-1)%/%cols + 1]^2)*fit$loadings[i]
-	cat(v)
+	cat(as.numeric(variances[(i-1)%/%cols + 1])*fit$loadings[i])
 	cat(" ")
 }
 cat("\n")
