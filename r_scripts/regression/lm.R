71c8bf3594c85097c1a226b469cacd89ec2b7dd5
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2016-10-24 11:52:32 +0300

    added regression script using multidimensional linear regression model

---
+++ r_scripts/regression/lm.R
@@ -0,0 +1,21 @@
+#!/usr/bin/Rscript
+# author: ggian
+# date: Mon Oct 24 11:40:15 EEST 2016
+#
+# Script used to create a linear model used for regression
+
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) < 2 ) { 
+	cat("Please provide the training set and test sets.\n") 
+	quit()
+}
+
+datafile <- args[1]
+testfile <- args[2]
+train.data <- read.csv(datafile)
+test.data <- read.csv(testfile)
+
+fit <- lm(class ~ ., data=train.data)
+pred <- predict(fit, test.data)
+cat(sum((pred-test.data$class)^2)/length(test.data))
