9927d593eb487cf7498869afae0451b393c408ba
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2016-12-07 12:29:52 +0200

    removed deprecated optimization classes

--- apps/doc.go
+++
@@ -1,4 +0,0 @@
-// This package contains various functions and structs that operate on the
-// similarity mattrices and any other low-level structs from the core package
-// and produce meta-results of high interest.
-package apps
--- apps/optimization/doc.go
+++
@@ -1,4 +0,0 @@
-// Package optimizer contains methods and structures used to traverse the
-// feature space of the datasets and find the best (or approximates of it)
-// available datasets.
-package optimization
