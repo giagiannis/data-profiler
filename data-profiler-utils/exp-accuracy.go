b24baa8b380c587c7b3a48a906d116c4c3fa9470
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2017-07-03 13:27:54 +0300

    finalized exp-accuracy utils function

--- data-profiler-utils/exp-accuracy.go
+++ data-profiler-utils/exp-accuracy.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -95,9 +96,6 @@ func expAccuracyRun() {
 	defer output.Close()
 
 	results := make(map[float64][]map[string]float64)
-	for _, sr := range params.samplingRates {
-		results[sr] = make([]map[string]float64, 0)
-	}
 
 	// threads configuration
 	sync := make(chan bool, *params.threads)
@@ -116,10 +114,59 @@ func expAccuracyRun() {
 	noResults := *params.repetitions * len(params.samplingRates)
 	for i := 0; i < noResults; i++ {
 		v := <-resChannel
+		if _, ok := results[v.sr]; !ok {
+			results[v.sr] = make([]map[string]float64, 0)
+		}
 		results[v.sr] = append(results[v.sr], v.res)
+
 	}
 
-	log.Println(results)
+	keys := writeResults(output, results, params.samplingRates)
+	fmt.Println("Column names/indices:")
+	fmt.Printf("%d - %s\n", 1, "sr")
+	for i, k := range keys {
+		fmt.Printf("%d - %s\n", i+2, k)
+	}
+
+}
+
+// writeResults writes the results to the output file and returns a string slice
+// containing the names of the CSV's columns
+func writeResults(output *os.File, results map[float64][]map[string]float64, samplingRates []float64) []string {
+	keys, keysFinal := make([]string, 0), make([]string, 0)
+	getValue := func(key string, results []map[string]float64) []float64 {
+		res := make([]float64, 0)
+		for _, v := range results {
+			res = append(res, v[key])
+		}
+		return res
+	}
+	for _, sr := range samplingRates {
+		rLine := results[sr]
+		if len(keys) == 0 { // get and print header
+			for k := range rLine[0] {
+				keys = append(keys, k)
+			}
+			sort.Strings(keys)
+			fmt.Fprintf(output, "sr")
+			for _, k := range keys {
+				for _, k2 := range []string{"mean", "stddev", "median"} {
+					fmt.Fprintf(output, "\t%s", k+"-"+k2)
+					keysFinal = append(keysFinal, k+"-"+k2)
+				}
+			}
+			fmt.Fprintf(output, "\n")
+		}
+
+		fmt.Fprintf(output, "%.2f", sr)
+		for _, k := range keys {
+			values := getValue(k, rLine)
+			mean, stddev, median := core.Mean(values), core.StdDev(values), core.Percentile(values, 50)
+			fmt.Fprintf(output, "\t%.5f\t%.5f\t%.5f", mean, median, stddev)
+		}
+		fmt.Fprintf(output, "\n")
+	}
+	return keysFinal
 }
 
 type resChannelResult struct {
@@ -132,10 +179,12 @@ func runModeler(sr float64, modeler core.Modeler, sync chan bool, resChannel cha
 	err := modeler.Run()
 	if err != nil {
 		log.Println(err)
+		sync <- true
+		return
 	}
 	res := modeler.ErrorMetrics()
-	res["execTime"] = modeler.ExecTime()
-	res["evalTime"] = modeler.EvalTime()
+	res["TimeExec"] = modeler.ExecTime()
+	res["TimeEval"] = modeler.EvalTime()
 	resChannel <- resChannelResult{sr, res}
 	sync <- true
 }
